ed27d4a95dd786684544911e63788d491259e388
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-08-02 13:27:28 +0700

    Fixed decoding

--- js/rdfloader.js
+++ js/rdfloader.js
@@ -1,17 +1,26 @@
+var activefile = {};
+var ext_db;
+
 function readFile(input, mode) {
-    var datablock;
+    var datablock = [];
     if (mode == "D" || mode == "M" || mode == "S") {
         chunkBreak(input, datablock, 4);
         if (validate(datablock, mode) == true) {
-
+            activefile.A = decodeDecimal(datablock, 3, 4);
+            activefile.B = decodeDecimal(datablock, 8, 1);
+            activefile.C = decodeDecimal(datablock, 10, 13);
         }
     } else {
         console.log("Unknown file type '" + mode + "', aborting!");
     }
+    ext_db = datablock;
 }
 function chunkBreak(input, output, length) {
     for (var i = 0; i < input.length; i++) {
-        output[Math.floor((i + 1) / length)] += input[i];
+        if (i % length === 0) {
+            output[Math.floor((i) / length)] = "";
+        }
+        output[Math.floor(i / length)] += input[i];
     }
 }
 function validate(input, mode) {
@@ -26,4 +35,11 @@ function validate(input, mode) {
             console.log("Unknown file type '" + mode + "', aborting!");
             break;
     }
+}
+function decodeDecimal(input, start, length) {
+    var result = "";
+    for (var i = start; i < start + length; i++) {
+        result += String.fromCharCode(parseInt(input[i], 10));
+    }
+    return result;
 }
