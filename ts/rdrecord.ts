ddc6b5636dacb279c61fb857a9705ad851468b30
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-08-07 16:40:51 +0700

    Added init for RDRecord

--- ts/rdrecord.ts
+++ ts/rdrecord.ts
@@ -1,11 +1,25 @@
 class RDRecord {
-    Entry = -1;
-    ID = -1;
-    Prefix = "";
-    FirstName = "";
-    LastName = "";
-    Date = new Date();
-    Amount = -1;
-    Tax = -1;
-    Conditions = RDCondition.NONE;
+    Entry: number;
+    ID: number;
+    Prefix: string;
+    FirstName: string;
+    LastName: string;
+    Date: Date;
+    Amount: number;
+    Tax: number;
+    Conditions: RDCondition;
+    private init(): void {
+        this.Entry = -1;
+        this.ID = -1;
+        this.Prefix = "";
+        this.FirstName = "";
+        this.LastName = "";
+        this.Date = new Date();
+        this.Amount = -1;
+        this.Tax = -1;
+        this.Conditions = RDCondition.NONE;
+    }
+    constructor() {
+        this.init();
+    }
 }
