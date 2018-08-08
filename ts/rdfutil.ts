7020794c636f5828f8878cc7b3f1dceefc0dc71a
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-08-08 18:26:22 +0700

    Removed unused functions

--- ts/rdfutil.ts
+++ ts/rdfutil.ts
@@ -8,36 +8,6 @@ class RDFUtil {
         this.data.LoadFromRaw(input);
         this.file.LoadFromData(this.data);
     }
-    private chunkBreak(input: string, length: number): Array<string> {
-        let output: Array<string> = new Array<string>();
-        for (let i = 0; i < input.length; i++) {
-            if (i % length === 0) {
-                output.push("");
-            }
-            output[Math.floor(i / length)] += input.charAt(i);
-        }
-        return output;
-    }
-    private separateEntries(input: Array<string>): Array<Array<string>> {
-        let output = new Array<Array<string>>();
-        let occurrences = new Array<number>();
-        for (let i = 0; i < input.length; i++) {
-            if (input[i] == "0124" &&
-                input[i + 1] == "0124" &&
-                input[i + 2] == "0124") {
-                occurrences[occurrences.length] = i;
-                i += 2;
-            }
-        }
-        occurrences[occurrences.length] = input.length;
-        for (let i = 0; i < occurrences.length - 1; i++) {
-            output[i] = new Array<string>();
-            for (var j = occurrences[i]; j < occurrences[i + 1]; j++) {
-                output[i].push(input[j]);
-            }
-        }
-        return output;
-    }
     private validate(): boolean {
         return true;
     }
